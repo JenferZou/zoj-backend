0b881cb00def16772bbf3e12fff5f595852e2648
Jenfer <1634114623@qq.com>
2024-08-17 21:56:23 +0800

    基本增删改查

---
+++ src/main/java/com/jenfer/zoj/model/dto/questionSubmit/QuestionSubmitAddRequest.java
@@ -0,0 +1,41 @@
+package com.jenfer.zoj.model.dto.questionSubmit;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 创建请求
+ *
+ 
+ */
+@Data
+public class QuestionSubmitAddRequest implements Serializable {
+
+    /**
+     * 编程语言
+     */
+    private String language;
+
+    /**
+     * 用户代码
+     */
+    private String code;
+
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+
+
+
+
+    private static final long serialVersionUID = 1L;
+}
